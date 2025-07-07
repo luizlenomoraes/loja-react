a54ab257cefd85b97f3d43b46ae3e8b86954a767
luizlenomoraes <grupovipiqoption@gmail.com>
2025-07-07 12:10:12 -0300

    Update main.jsx
    versão gemini

--- futcamisas2/src/main.jsx
+++ futcamisas2/src/main.jsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { AuthProvider } from './contexts/AuthContext';
+import { ProductProvider } from './contexts/ProductContext';
+import { CartProvider } from './contexts/CartContext';
 
+// Renderiza a aplicação, envolvendo o componente App com todos os provedores de contexto
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <ProductProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductProvider>
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
-); 
+);
